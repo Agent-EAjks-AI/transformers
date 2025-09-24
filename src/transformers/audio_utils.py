8df6410888526c81211f38c2a0a924f39cb6312a
ydshieh <ydshieh@users.noreply.github.com>
2025-09-24 15:42:32 +0200

    PR merged

--- src/transformers/audio_utils.py
+++ src/transformers/audio_utils.py
@@ -25,8 +25,10 @@ from collections.abc import Sequence
 from io import BytesIO
 from typing import Any, Optional, Union
 
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+import httpx
 import numpy as np
-import requests
 from packaging import version
 
 from .utils import (
@@ -38,6 +40,9 @@ from .utils import (
     requires_backends,
 )
 
+
+if TYPE_CHECKING:
+    import torch
 
 if is_soundfile_available():
     import soundfile as sf
@@ -131,7 +136,9 @@ def load_audio_librosa(audio: Union[str, np.ndarray], sampling_rate=16000, timeo
 
     # Load audio from URL (e.g https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/translate_to_chinese.wav)
     if audio.startswith("http://") or audio.startswith("https://"):
-        audio = librosa.load(BytesIO(requests.get(audio, timeout=timeout).content), sr=sampling_rate)[0]
+        audio = librosa.load(
+            BytesIO(httpx.get(audio, follow_redirects=True, timeout=timeout).content), sr=sampling_rate
+        )[0]
     elif os.path.isfile(audio):
         audio = librosa.load(audio, sr=sampling_rate)[0]
     return audio
@@ -173,7 +180,7 @@ def load_audio_as(
         # Load audio bytes from URL or file
         audio_bytes = None
         if audio.startswith(("http://", "https://")):
-            response = requests.get(audio, timeout=timeout)
+            response = httpx.get(audio, follow_redirects=True, timeout=timeout)
             response.raise_for_status()
             audio_bytes = response.content
         elif os.path.isfile(audio):
